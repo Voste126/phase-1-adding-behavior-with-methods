a035ad3b359ea20d0a0854be79917d8704f1b4e9
voste126 <steveaustine126@gmail.com>
2023-07-12 13:52:33 +0300

    second class od OPP

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Your code here
+//cat = new Cat("Sasha", "female")
+class cat{
+    constructor(name,sex){
+        this.name = name;
+        this.sex= sex;
+        this.speak = () => {
+            return `${name} says meow!`
+        }
+    }
+}
+class Dog{
+    constructor(name,sex){
+        this.name = name;
+        this.sex= sex;
+        this.speak = () => {
+            return `${name} says woof!`
+        }
+    }
+}
+class Bird{
+    constructor(name,sex){
+        this.name = name;
+        this.sex= sex;
+        this.speak = () => {
+            return ( sex === 'male')? `It's me! ${this.name}, the parrot!`: `${this.name} says squawk!`
+        }
+    }
+
+}
